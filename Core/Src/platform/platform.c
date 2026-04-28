19cc798e287e7363133d726652f62c6a53ab1648
Rrrrandanna <kristina.kochetkova6853@gmail.com>
2026-04-28 11:24:28 +0300

    lab2 completed 2

--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -2,14 +2,15 @@
 #include "platform.h"
 #include "main.h"
 //#include "lab1_gpio/lab1_gpio.h"
-//#include "lab2_timer/lab2_timer.h"
+#include "lab2_timer/lab2_timer.h"
 //#include "lab3_stepper/lab3_stepper.h"
-#include "lab4_adc/lab4_adc.h"
+//#include "lab4_adc/lab4_adc.h"
 extern UART_HandleTypeDef huart1;
 /* Однократный вызов */
 int plt_init(void)
 {
-
+	  plt_timer_set(200);
+	    plt_timer_start_irq();
 	return 0;
 }
 
@@ -17,9 +18,13 @@ int plt_init(void)
 /* Повторяющийся вызов */
 void plt_process(void)
 {
-	/* Устанавливаем задержку */
-	plt_delay(500);
+
 }
+void plt_timer_irq_cb(void)
+{
+    HAL_GPIO_TogglePin(USER_LED_GPIO_Port, USER_LED_Pin);
+}
+
 
 /* Перенести в main.c в user code */
 //void plt_delay(uint32_t delay_ms)
